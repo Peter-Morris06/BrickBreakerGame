d64b63fa9635ac6df4a871881f23c1bd5a37c3c3
Peter-Morris06 <149684447+Peter-Morris06@users.noreply.github.com>
2024-04-24 08:42:05 -0400

    Add files via upload

--- BlockSpawningSC.cs
+++ BlockSpawningSC.cs
@@ -6,8 +6,12 @@ public class BlockSpawningSC : MonoBehaviour
 {
     public GameObject block;
     GameObject currentBlock;
+    public GameObject ball;
+
     public bool spawnTime;
-    
+    public bool moveforwardTIME;
+
+
 
 
     void Start()
@@ -17,17 +21,31 @@ public class BlockSpawningSC : MonoBehaviour
 
     void Update()
     {
+        if(moveforwardTIME == true)
+        {
+            block.GetComponent<BlockSC>().Movingforward();
+            moveforwardTIME = false;
+
+        }
+
 
 
         if (spawnTime == true)
         {
+
+
+
             for (int i = Random.Range(0, 12); i < 12; i++)
             {
                 currentBlock = Instantiate(block, new Vector3(((Random.Range(-6, 6)) * 2 + 1), 11f, -2f), Quaternion.identity);               
             }
             spawnTime = false;
+            moveforwardTIME = true;
         }
-            
-                
+
+    
+
+
     }
+    
 }
